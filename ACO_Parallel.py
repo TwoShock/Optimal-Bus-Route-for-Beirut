afc7ff26ae3fc5edecd33626977dae92a7ab280f
OmarJaroudi <30807936+OmarJaroudi@users.noreply.github.com>
2020-05-03 18:30:32 +0300

    Cleaned up code

--- ACO_Parallel.py
+++ ACO_Parallel.py
@@ -19,213 +19,212 @@ mod = SourceModule("""
 #include <curand.h>
 #include <curand_kernel.h>
 
-                   
 #define index(length, line, column) (column + line * length) 
-
-extern "C" {                  
-    __global__ void cuda_evaporate(double * pheromones, int n_cities, double evap_rate) { 
-    
-    	int edge_id = threadIdx.x + blockIdx.x * blockDim.x; 
-    	pheromones[edge_id] =pheromones[edge_id]* evap_rate;
-    } 
-    
-    __global__ void cuda_reinforce(double * pheromones, int * distances, int * path, int n_cities, double amount) {
-    
-    	int col_id = threadIdx.x + blockIdx.x * blockDim.x;
-    
-    	int origin = path [col_id];
-    	int dest = path [col_id + 1];
-        int idx = index(n_cities, origin, dest);
-    	pheromones[idx] = pheromones[idx] + amount;
-    	pheromones [index(n_cities, dest, origin)] = pheromones [index(n_cities, dest, origin)]+amount; 
+extern "C" {
+    __global__ void evaporate(double* pheromoneValues, int numberOfCities, double evaporationRate) {
+        /*
+        This function modifies the pheremone trail to account for the evaporation constant
+        */
+        int edgeID = threadIdx.x + blockIdx.x * blockDim.x;
+        pheromoneValues[edgeID] = pheromoneValues[edgeID] * evaporationRate;
     }
-    
-    __global__ void cuda_construct_tour(int * tours, int * visited, double * choiceinfo, double * probs, int n_cities) {
-    
-    	int line_id = blockDim.x * blockIdx.x + threadIdx.x; 
-    
-    	for (int step = 1; step <n_cities; step++) { 
-    
-    		int current = tours[index(n_cities, line_id, step - 1)];
-    		double sum_probs = 0.0;
-    
-    		for (int i = 0; i <n_cities; i ++) {
-    			if (visited [index(n_cities, line_id, i)] == 1) 
-    				probs [index(n_cities, line_id, i)] = 0.0;
-    			else {
-    				double current_prob = choiceinfo[index(n_cities, current, i)]; 
-    				probs [index(n_cities, line_id, i)] = current_prob; 
-    				sum_probs = sum_probs+current_prob; 
-    			}
-    		}
-    
-    		double random;
-    		curandState_t state;
-    		curand_init ((unsigned long long) clock(), 0, 0, & state); 
-    		random = curand_uniform(& state); 
-    		random = random * sum_probs; 
-    
-    		int next;
-    		double sum = probs [index(n_cities, line_id, 0)];
-    
-    		for (next = 0; sum <random; next ++) {
-    			sum  = sum + probs [index(n_cities, line_id, next + 1)];
-    		} 
-    
-    		tours [index(n_cities, line_id, step)] = next; 
-    		visited [index(n_cities, line_id, next)] = 1;
-    	} 
+
+    __global__ void reinforcePath(double* pheromoneValues, int* distances, int* pathArray, int numberOfCities, double reinforcementAmount) {
+
+        int currentXThread = threadIdx.x + blockIdx.x * blockDim.x;
+
+        int source = pathArray[currentXThread];
+        int dest = pathArray[currentXThread + 1];
+        int idx = index(numberOfCities, source, dest);
+        pheromoneValues[idx] = pheromoneValues[idx] + reinforcementAmount;
+        pheromoneValues[index(numberOfCities, dest, source)] = pheromoneValues[index(numberOfCities, dest, source)] + reinforcementAmount;
+    }
+
+    __global__ void constructTour(int* tourArray, int* visitedArray, double* choiceInfo, double* probabilityArray, int numberOfCities) {
+
+        int currentXThread = blockDim.x * blockIdx.x + threadIdx.x;
+
+        for (int step = 1; step < numberOfCities; step++) {
+
+            int current = tourArray[index(numberOfCities, currentXThread, step - 1)];
+            double currentProbabilitySum = 0.0;
+
+            for (int i = 0; i < numberOfCities; i++) {
+                if (visitedArray[index(numberOfCities, currentXThread, i)] == 1)
+                    probabilityArray[index(numberOfCities, currentXThread, i)] = 0.0;
+                else {
+                    double currentProbability = choiceInfo[index(numberOfCities, current, i)];
+                    probabilityArray[index(numberOfCities, currentXThread, i)] = currentProbability;
+                    currentProbabilitySum = currentProbabilitySum + currentProbability;
+                }
+            }
+
+            double random;
+            curandState_t state;
+            curand_init((unsigned long long) clock(), 0, 0, &state);
+            random = curand_uniform(&state);
+            random = random * currentProbabilitySum;
+
+            int next;
+            double sum = probabilityArray[index(numberOfCities, currentXThread, 0)];
+
+            for (next = 0; sum < random; next++) {
+                sum += probabilityArray[index(numberOfCities, currentXThread, next + 1)];
+            }
+
+            tourArray[index(numberOfCities, currentXThread, step)] = next;
+            visitedArray[index(numberOfCities, currentXThread, next)] = 1;
+        }
     }
 }
 """,no_extern_c=True)
 
 
 
-#define randdouble () ((double) rand () / (double) RAND_MAX) 
-
-Infinity = 65536 
-NUMBER_OF_ITERATIONS = 50
-INIT_PHEROMONE_AMOUNT = 1.0
-EVAPORATION_RATE = 0.5
-ALFA = 1
+INF = 65536 
+ITERATIONS = 50
+INTIALPHEREMONEAMOUNT = 1.0
+EVAPORATION_CONSTANT = 0.5
+ALPHA = 1
 BETA = 2
+
 
 def index(length, line, column):
     return (column + line * length) 
 
-def threads( n_ants):
+def threads(numberOfAnts):
     n_threds = 1
-    while(n_threds * 2 <n_ants):
+    while(n_threds * 2 <numberOfAnts):
         n_threds*= 2
     return n_threds
 
-def thread_per_block(n_ants):
-	blocks = math.log(n_ants)
+def threadsPerBlock(numberOfAnts):
+	blocks = math.log(numberOfAnts)
 	return math.pow(2, blocks)
 
-def calculate_pathcost (distances,path, n_cities):
-    cost = 0
-    for count in range(n_cities-1):
-        idx = index(n_cities, path [count], path [count + 1])
-        if idx<Infinity:
-            cost += distances[idx]
-        else:
-            cost+=Infinity
-    return cost 
-
-def best_solution (tours, distances, n_ants, n_cities):
-    best_tour = tours
-    for tour in range(n_ants):
-        bestCost = calculate_pathcost(distances, best_tour, n_cities)
-        currentCost = calculate_pathcost(distances,tours [index (n_cities, tour, 0):], n_cities)
-        if (currentCost<bestCost): 
-            best_tour = tours[index(n_cities, tour, 0):]
-    return best_tour
-
-
-def evaporate (pheromones,n_cities):
-    pheromones_device = np.empty_like(pheromones)
+def evaporate (pheromones,numberOfCities):
+    pheremoneDeviceArray = np.empty_like(pheromones)
     
-    pheromones_device = cuda.mem_alloc(pheromones.nbytes)
-    cuda.memcpy_htod(pheromones_device,pheromones)
-    func = mod.get_function("cuda_evaporate")
-    func(pheromones_device, np.int32(n_cities), np.int32(EVAPORATION_RATE),block = (n_cities,1,1),grid = (n_cities,1))
-    cuda.memcpy_dtoh(pheromones,pheromones_device)
+    pheremoneDeviceArray = cuda.mem_alloc(pheromones.nbytes)
+    cuda.memcpy_htod(pheremoneDeviceArray,pheromones)
+    func = mod.get_function("evaporate")
+    func(pheremoneDeviceArray, np.int32(numberOfCities), np.int32(EVAPORATION_CONSTANT),block = (numberOfCities,1,1),grid = (numberOfCities,1))
+    cuda.memcpy_dtoh(pheromones,pheremoneDeviceArray)
     return pheromones
 
-def reinforce(pheromones,distances, path,n_cities):
-    amount = float(1.0 / calculate_pathcost (distances, path, n_cities))
+def reinforce(pheromones,distanceArray, path,numberOfCities):
+    amount = float(1.0 / calcPathCost (distanceArray, path, numberOfCities))
     
     path_device = np.empty_like(path)
-    distances_device = np.empty_like(distances)
-    pheromones_device = np.empty_like(pheromones)
+    distanceArray_device = np.empty_like(distanceArray)
+    pheremoneDeviceArray = np.empty_like(pheromones)
     
     path_device = cuda.mem_alloc(path.nbytes)
-    distances_device = cuda.mem_alloc(distances.nbytes)
-    pheromones_device = cuda.mem_alloc(pheromones.nbytes)
+    distanceArray_device = cuda.mem_alloc(distanceArray.nbytes)
+    pheremoneDeviceArray = cuda.mem_alloc(pheromones.nbytes)
 
     cuda.memcpy_htod(path_device,path)
-    cuda.memcpy_htod(distances_device,distances)
-    cuda.memcpy_htod(pheromones_device,pheromones)
+    cuda.memcpy_htod(distanceArray_device,distanceArray)
+    cuda.memcpy_htod(pheremoneDeviceArray,pheromones)
     
-    func = mod.get_function("cuda_reinforce")
-    func(pheromones_device, distances_device, path_device, np.int32(n_cities),np.float32(amount),block=(1,1,1),grid=(n_cities,1))
+    func = mod.get_function("reinforcePath")
+    func(pheremoneDeviceArray, distanceArray_device, path_device, np.int32(numberOfCities),np.float32(amount),block=(1,1,1),grid=(numberOfCities,1))
     
-    cuda.memcpy_dtoh(distances,distances_device)
-    cuda.memcpy_dtoh(pheromones,pheromones_device)
+    cuda.memcpy_dtoh(distanceArray,distanceArray_device)
+    cuda.memcpy_dtoh(pheromones,pheremoneDeviceArray)
     
-    return (pheromones,distances)
+    return (pheromones,distanceArray)
+
+
+def calcPathCost (distanceArray,path, numberOfCities):
+    cost = 0
+    for count in range(numberOfCities-1):
+        idx = index(numberOfCities, path [count], path [count + 1])
+        if idx<INF:
+            cost += distanceArray[idx]
+        else:
+            cost+=INF
+    return cost 
+
+
+def bestSolution (tours, distanceArray, numberOfAnts, numberOfCities):
+    bestTour = tours
+    for tour in range(numberOfAnts):
+        bestCost = calcPathCost(distanceArray, bestTour, numberOfCities)
+        currentCost = calcPathCost(distanceArray,tours [index (numberOfCities, tour, 0):], numberOfCities)
+        if (currentCost<bestCost): 
+            bestTour = tours[index(numberOfCities, tour, 0):]
+    return bestTour
 	
 
-def run (distances, n_cities, n_ants):
+def run (distanceArray, numberOfCities, numberOfAnts):
     
-    pheromones = np.empty((n_cities*n_cities,),dtype=np.float32)
-    tours = np.empty((n_cities*n_ants,),dtype=int)
-    visited = np.empty((n_cities*n_ants,),dtype=int)
-    choiceinfo = np.empty((n_cities*n_cities,),dtype=np.float32)
+    pheromones = np.empty((numberOfCities*numberOfCities,),dtype=np.float32)
+    tours = np.empty((numberOfCities*numberOfAnts,),dtype=int)
+    visited = np.empty((numberOfCities*numberOfAnts,),dtype=int)
+    choiceInfo = np.empty((numberOfCities*numberOfCities,),dtype=np.float32)
     
     
-    distances_device = np.empty_like(distances)
+    distanceArray_device = np.empty_like(distanceArray)
     tours_device = np.empty_like(tours)
     visited_device = np.empty_like(visited)
-    choiceinfo_device = np.empty_like(choiceinfo)
-    probs = np.empty_like(pheromones)
+    choiceInfo_device = np.empty_like(choiceInfo)
+    probabilityArray = np.empty_like(pheromones)
     
-    distances_device = cuda.mem_alloc(distances.nbytes)
+    distanceArray_device = cuda.mem_alloc(distanceArray.nbytes)
     tours_device = cuda.mem_alloc(tours.nbytes)
     visited_device = cuda.mem_alloc(visited.nbytes)
-    choiceinfo_device = cuda.mem_alloc(choiceinfo.nbytes)
-    probs = cuda.mem_alloc(pheromones.nbytes)
+    choiceInfo_device = cuda.mem_alloc(choiceInfo.nbytes)
+    probabilityArray = cuda.mem_alloc(pheromones.nbytes)
 
-    cuda.memcpy_htod(distances_device,distances)
+    cuda.memcpy_htod(distanceArray_device,distanceArray)
     
-    for i in range(n_cities):
-        for j in range(n_cities):
-            pheromones [index (n_cities, i, j)] = INIT_PHEROMONE_AMOUNT
+    for i in range(numberOfCities):
+        for j in range(numberOfCities):
+            pheromones [index (numberOfCities, i, j)] = INTIALPHEREMONEAMOUNT
             
-    for iteration in range(NUMBER_OF_ITERATIONS):
+    for iteration in range(ITERATIONS):
 
-        for i in range(n_ants):
-            for j in range(n_cities):
-                tours [index(n_cities, i, j)] = Infinity 
+        for i in range(numberOfAnts):
+            for j in range(numberOfCities):
+                tours [index(numberOfCities, i, j)] = INF 
         
-        for i in range(n_ants):
-            for j in range(n_cities):
-                visited [index (n_cities, i, j)] = 0 
+        for i in range(numberOfAnts):
+            for j in range(numberOfCities):
+                visited [index (numberOfCities, i, j)] = 0 
                 
-        for i in range(n_cities):
-            for j in range(n_cities):
-                edge_pherom = pheromones [index (n_cities, i, j)]
-                edge_weight = distances [index (n_cities, i, j)]
+        for i in range(numberOfCities):
+            for j in range(numberOfCities):
+                edge_pherom = pheromones [index (numberOfCities, i, j)]
+                edge_weight = distanceArray [index (numberOfCities, i, j)]
                 prob = 0.0
                 if (edge_weight != 0):
-                    prob = math.pow(edge_pherom, ALFA) * math.pow((1 / edge_weight), BETA)
+                    prob = math.pow(edge_pherom, ALPHA) * math.pow((1 / edge_weight), BETA)
                 else:
-                    prob = math.pow(edge_pherom, ALFA) * math.pow(Infinity, BETA)
-                choiceinfo [index (n_cities, i, j)] = prob
+                    prob = math.pow(edge_pherom, ALPHA) * math.pow(INF, BETA)
+                choiceInfo [index (numberOfCities, i, j)] = prob
         
-        cuda.memcpy_htod(choiceinfo_device,choiceinfo)
+        cuda.memcpy_htod(choiceInfo_device,choiceInfo)
         
-        for ant in range(n_ants):
-            init = int(random.uniform(0,n_cities))
-            tours[index(n_cities, ant, 0)] = init
-            visited [index (n_cities, ant, init)] = 1
+        for ant in range(numberOfAnts):
+            init = int(random.uniform(0,numberOfCities))
+            tours[index(numberOfCities, ant, 0)] = init
+            visited [index (numberOfCities, ant, init)] = 1
         
         cuda.memcpy_htod(visited_device,visited)
         cuda.memcpy_htod(tours_device,tours)
         
-        gridDim = int(n_ants / thread_per_block(n_ants))
-        antsPerBlock = int(thread_per_block (n_ants))
+        gridDim = int(numberOfAnts / threadsPerBlock(numberOfAnts))
+        antsPerBlock = int(threadsPerBlock (numberOfAnts))
         
-        func = mod.get_function("cuda_construct_tour")
-        func(tours_device, visited_device, choiceinfo_device, probs, np.int32(n_cities),block = (antsPerBlock,1,1),grid = (gridDim,1))
+        func = mod.get_function("constructTour")
+        func(tours_device, visited_device, choiceInfo_device, probabilityArray, np.int32(numberOfCities),block = (antsPerBlock,1,1),grid = (gridDim,1))
         
         cuda.memcpy_dtoh(tours,tours_device)
         cuda.memcpy_dtoh(visited,visited_device)
         
-        pheromones = evaporate (pheromones, n_cities)
-        best = best_solution (tours, distances, n_ants, n_cities)
-        pheromones,distances = reinforce (pheromones, distances, best, n_cities) 
-    best = best_solution (tours, distances, n_ants, n_cities)
+        pheromones = evaporate (pheromones, numberOfCities)
+        best = bestSolution (tours, distanceArray, numberOfAnts, numberOfCities)
+        pheromones,distanceArray = reinforce (pheromones, distanceArray, best, numberOfCities) 
+    best = bestSolution (tours, distanceArray, numberOfAnts, numberOfCities)
     return best 
-    
