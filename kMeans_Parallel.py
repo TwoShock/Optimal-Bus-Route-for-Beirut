1b0c4f3ee9a1003031440997b2159cc56545226d
OmarJaroudi <30807936+OmarJaroudi@users.noreply.github.com>
2020-05-05 17:11:56 +0300

    clean code

--- kMeans_Parallel.py
+++ kMeans_Parallel.py
@@ -3,12 +3,8 @@ from pycuda.compiler import SourceModule
 import pycuda.autoinit
 import numpy as np
 import math
-import matplotlib.pyplot as plt
-import osmnx as ox
-from sklearn.cluster import KMeans
 import random
-import time
-import pickle
+
 
 
 TPB = 32
@@ -16,108 +12,94 @@ K = 26
 MAX_ITER = 500
 
 mod = SourceModule("""
+
 #include <stdio.h>
 
 #define TPB 32
-#define K 26
-#define MAX_ITER 10
+#define CLUSTER_COUNT 26 // this is the k parameter in kmeans
 
-__device__ float distance(float x1, float x2,float y1, float y2)
-{
-	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+
+__device__ float distance(float x1, float x2, float y1, float y2){
+	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
-__global__ void kMeansClusterAssignment(float *d_datapoints_X,float *d_datapoints_Y, int *d_clust_assn, float *d_centroids_X,float *d_centroids_Y,int N)
-{
-	//get idx for this datapoint
-	const int idx = blockIdx.x*blockDim.x + threadIdx.x;
-
-	//bounds check
-	if (idx >= N) return;
+__global__ void AssignClusters(float* d_X, float* d_Y, int* d_assigned_cluster, float* d_centroids_X, float* d_centroids_Y, int N){
+	/*
+	Input:
+		d_X: array containing the X coordinates stored on the device 
+		d_Y: array containing the Y coordinates stored on the device
+		d_assigned_cluster: array which will contain the clusters that each point belong to
+		d_centroids_X: array containing the centroids for X
+		d_centroids_Y: array containing the centroids for Y
+		N: number of points
+	Function: Populates the d_assigned_cluster array with the correct values. Namely each point will belong to a given cluster and that is represented in the d_assigne_cluster array
+		ex: Assume the output of the function gives the following for an array of 6 points with 2 clusters
+			d_assignned_cluster = {0,0,0,1,1,1}
+			This means that points 0->2 are part of cluster 0 and points 2->5 are part of cluster 1
+		Each thread computes and assigns a single point to a cluster array so each thread works on one index of d_assigned_cluster
+	*/
+	const int i = blockIdx.x * blockDim.x + threadIdx.x;//computes global thread index
+	if (i >= N) return; //checks if given thread is out of bounds
 
 	//find the closest centroid to this datapoint
-	float min_dist = INFINITY;
-	int closest_centroid = 0;
-
-	for(int c = 0; c<K;++c)
-	{
-		float dist = distance(d_datapoints_X[idx],d_centroids_X[c],d_datapoints_Y[idx],d_centroids_Y[c]);
-
-		if(dist < min_dist)
-		{
-			min_dist = dist;
-			closest_centroid=c;
+	float minDist = INFINITY;
+	int nearestCentroid = 0;//start out assuming the nearest is cluster 0
+	for (int centroidIndex = 0; centroidIndex < CLUSTER_COUNT; ++centroidIndex){
+		float dist = distance(d_X[i], d_centroids_X[centroidIndex], d_Y[i], d_centroids_Y[centroidIndex]);//compute distance from the current point and the current centroid choosen
+		if (dist < minDist){ //updates the nearestCentroid field if the distance is smaller than the previous distance
+			minDist = dist;
+			nearestCentroid = centroidIndex;
 		}
 	}
-
 	//assign closest cluster id for this datapoint/thread
-	d_clust_assn[idx]=closest_centroid;
-}
-        
-__global__ void kMeansFinalStep(float *d_centroids_X,float *d_centroids_Y,int *d_clust_sizes){
-    int idx = blockIdx.x*blockDim.x + threadIdx.x;
-    if (idx<K){
-        d_centroids_X[idx] = d_centroids_X[idx]/d_clust_sizes[idx];
-        d_centroids_Y[idx] = d_centroids_Y[idx]/d_clust_sizes[idx];
-
-    }
-
+	d_assigned_cluster[i] = nearestCentroid;
 }
 
-
-__global__ void kMeansCentroidUpdate(float *d_datapoints_X, float *d_datapoints_Y, int *d_clust_assn, float *d_centroids_X,float *d_centroids_Y, int *d_clust_sizes,int N)
-{
-
-	//get idx of thread at grid level
-	const int idx = blockIdx.x*blockDim.x + threadIdx.x;
-
-	//bounds check
-	if (idx >= N) return;
-
+__global__ void FinalStep(float* d_centroids_X, float* d_centroids_Y, int* d_clust_sizes) {
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < CLUSTER_COUNT) {
+		d_centroids_X[idx] = d_centroids_X[idx] / d_clust_sizes[idx];
+		d_centroids_Y[idx] = d_centroids_Y[idx] / d_clust_sizes[idx];
+	}
+}
+__global__ void UpdateCentroids(float* d_X, float* d_Y, int* d_assigned_cluster, float* d_centroids_X, float* d_centroids_Y, int* d_clust_sizes, int N){
+	/*
+	Function: Recomputes the new centroids given current d_X d_Y 
+	*/
+	const int i = blockIdx.x * blockDim.x + threadIdx.x;//computes global thread index
+	if (i >= N) return;//checks if thread is out of bounds
 	//get idx of thread at the block level
 	const int s_idx = threadIdx.x;
-
 	//put the datapoints and corresponding cluster assignments in shared memory so that they can be summed by thread 0 later
-	__shared__ float s_datapoints_X[TPB];
-    __shared__ float s_datapoints_Y[TPB];
-	__shared__ int s_clust_assn[TPB];
-
-	s_datapoints_X[s_idx]= d_datapoints_X[idx];
-    s_datapoints_Y[s_idx]= d_datapoints_Y[idx];
-
-	s_clust_assn[s_idx] = d_clust_assn[idx];
-
+	__shared__ float s_X[TPB];
+	__shared__ float s_Y[TPB];
+	__shared__ int s_assigned_cluster[TPB];
+	s_X[s_idx] = d_X[i];
+	s_Y[s_idx] = d_Y[i];
+	s_assigned_cluster[s_idx] = d_assigned_cluster[i];
 	__syncthreads();
-
 	//it is the thread with idx 0 (in each block) that sums up all the values within the shared array for the block it is in
-	if(s_idx==0)
+	if (s_idx == 0)
 	{
-		float b_clust_datapoint_sums_X[K]={0};
-        float b_clust_datapoint_sums_Y[K]={0};
-
-		int b_clust_sizes[K]={0};
-
-		for(int j=0; j< blockDim.x; ++j)
+		float b_clust_datapoint_sums_X[CLUSTER_COUNT] = { 0 };
+		float b_clust_datapoint_sums_Y[CLUSTER_COUNT] = { 0 };
+		int b_clust_sizes[CLUSTER_COUNT] = { 0 };
+		for (int j = 0; j < blockDim.x; ++j)
 		{
-			int clust_id = s_clust_assn[j];
-			b_clust_datapoint_sums_X[clust_id]+=s_datapoints_X[j];
-            b_clust_datapoint_sums_Y[clust_id]+=s_datapoints_Y[j];
-
-			b_clust_sizes[clust_id]+=1;
+			int clust_id = s_assigned_cluster[j];
+			b_clust_datapoint_sums_X[clust_id] += s_X[j];
+			b_clust_datapoint_sums_Y[clust_id] += s_Y[j];
+			b_clust_sizes[clust_id] += 1;
 		}
-
 		//Now we add the sums to the global centroids and add the counts to the global counts.
-		for(int z=0; z < K; ++z)
+		for (int z = 0; z < CLUSTER_COUNT; ++z)
 		{
-			atomicAdd(&d_centroids_X[z],b_clust_datapoint_sums_X[z]);
-            atomicAdd(&d_centroids_Y[z],b_clust_datapoint_sums_Y[z]);
-
-			atomicAdd(&d_clust_sizes[z],b_clust_sizes[z]);
+			atomicAdd(&d_centroids_X[z], b_clust_datapoint_sums_X[z]);
+			atomicAdd(&d_centroids_Y[z], b_clust_datapoint_sums_Y[z]);
+			atomicAdd(&d_clust_sizes[z], b_clust_sizes[z]);
 		}
 	}
-
 	__syncthreads();
-
 }
 """)
 
@@ -160,7 +142,7 @@ def CudaKMeans (x_coord,y_coord,N):
     
     cur_iter = 1;
     while cur_iter<MAX_ITER:
-        func = mod.get_function("kMeansClusterAssignment")
+        func = mod.get_function("AssignClusters")
         func(d_datapoints_X,d_datapoints_Y,d_clust_assn,d_centroids_X,d_centroids_Y, np.int32(N),block=(TPB,1,1),grid=(math.ceil(N/TPB),1))
         cuda.memcpy_dtoh(h_clust_assn, d_clust_assn)
         
@@ -171,10 +153,10 @@ def CudaKMeans (x_coord,y_coord,N):
         temp = np.zeros((K,),dtype = int)
         cuda.memcpy_htod(d_clust_sizes,temp)
         
-        func = mod.get_function("kMeansCentroidUpdate")
+        func = mod.get_function("UpdateCentroids")
         func(d_datapoints_X,d_datapoints_Y,d_clust_assn,d_centroids_X,d_centroids_Y,d_clust_sizes, np.int32(N),block=(TPB,1,1),grid=(math.ceil(N/TPB),1))
     
-        func = mod.get_function("kMeansFinalStep")
+        func = mod.get_function("FinalStep")
         func(d_centroids_X,d_centroids_Y,d_clust_sizes, block=(TPB,1,1),grid=(math.ceil(K/TPB),1))
     
         cur_iter+=1
